1c163861f1e8c6823784fae8076ce8ea9b749ed5
ThatBaldDude4 <taylorv0004@gmail.com>
2026-08-20 20:24:26 -0500

    add script to create database

---
+++ db/populatedb.js
@@ -0,0 +1,31 @@
+const { Client } = require("pg");
+const { loadEnvFile } = require('node:process');
+loadEnvFile();
+const {HOST, USER, DATABASE, PASSWORD, PORT} = process.env;
+
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS usernames (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  username VARCHAR ( 255 )
+);
+
+INSERT INTO usernames (username) 
+VALUES
+  ('Bryan'),
+  ('Odin'),
+  ('Damon');
+`;
+
+async function main() {
+    console.log("seeding...");
+    const client = new Client({
+        connectionString: `postgresql://${USER}:${PASSWORD}@${HOST}:${PORT}/${DATABASE}`,
+    });
+    await client.connect();
+    await client.query(SQL);
+    await client.end();
+    console.log("done");
+};
+
+main();
